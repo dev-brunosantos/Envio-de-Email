060e3fff65785f5b0193ccd11dabb16efdaf576f
dev-brunosantos <brunosflauta@gmail.com>
2024-04-02 19:53:39 -0300

    Criação da API ( POST )

--- index.js
+++ index.js
@@ -16,20 +16,49 @@ const emailConfig = nodemailer.createTransport({
     }
 })
 
-const emailEnvio = {
-    from: "brunosdesenvolvedor@gmail.com",
-    to: "brunosflauta@gmail.com",
-    subject: "Teste de automação ",
-    html: "Testando aplicação de envio de e-mail automático com node."
-}
+// const emailEnvio = {
+//     from: "brunosdesenvolvedor@gmail.com",
+//     to: "brunosflauta@gmail.com",
+//     subject: "Teste de automação ",
+//     html: "Testando aplicação de envio de e-mail automático com node."
+// }
 
-emailConfig.sendMail(emailEnvio)
-    .then(() => {
-        return console.log('Email enviado com sucesso.')
-    })
-    .catch((erro) => {
-        return console.log('Tivemos um erro ao tentar enviar o email. ' + erro)
-    })
+// emailConfig.sendMail(emailEnvio)
+//     .then(() => {
+//         return console.log('Email enviado com sucesso.')
+//     })
+//     .catch((erro) => {
+//         return console.log('Tivemos um erro ao tentar enviar o email. ' + erro)
+//     })
+
+app.post('/enviar', (req, res) => {
+    try {
+
+        const emailEnvio = {
+            from: "",
+            to: "",
+            subject: "",
+            html: ""
+        }
+
+        emailEnvio.from = req.body.emissor
+        emailEnvio.to = req.body.remetente
+        emailEnvio.subject = req.body.titulo
+        emailEnvio.html = req.body.email
+
+        emailConfig.sendMail(emailEnvio)
+            .then(() => {
+                return res.json({'mensagem':'Email enviado com sucesso.'})
+            })
+            .catch((erro) => {
+                return res.json({'mensagem': 'Tivemos um erro ao tentar enviar o email. '})
+            })
+
+
+    } catch (error) {
+        res.send('Erro')
+    }
+})
 
 app.listen(port, () => {
     return console.log(`Servidor rodando em: ${url}`)
