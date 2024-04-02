7a48d09f16cd4e9b1a4bfae8866f0bf09e9fa84a
dev-brunosantos <brunosflauta@gmail.com>
2024-04-02 13:52:18 -0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const app = require("./src/app")
+const nodemailer = require('nodemailer')
+
+const link = process.env.LINK
+const port = process.env.PORT || 4000
+const url = `${link}:${port}`
+
+const emailConfig = nodemailer.createTransport({
+    host: process.env.HOST_EMAIL,
+    port: process.env.PORT_EMAIL,
+    secure: false,
+    auth: {
+        user: 'brunosdesenvolvedor@gmail.com',
+        pass: 'lcyx wicd flrp prmh'
+    }
+})
+
+const emailEnvio = {
+    from: "brunosdesenvolvedor@gmail.com",
+    to: "brunosflauta@gmail.com",
+    subject: "Teste de automação ",
+    html: "Testando aplicação de envio de e-mail automático com node."
+}
+
+emailConfig.sendMail(emailEnvio)
+    .then(() => {
+        return console.log('Email enviado com sucesso.')
+    })
+    .catch((erro) => {
+        return console.log('Tivemos um erro ao tentar enviar o email. ' + erro)
+    })
+
+app.listen(port, () => {
+    return console.log(`Servidor rodando em: ${url}`)
+})
